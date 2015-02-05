d805108c912a209b4119867bec66cfa0813a2f87
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-02-05 20:43:11 +0100

    import core/desc/* file into a module named 'spec' to relax some tasks. Note on main.js: maybe it isn't neccessary to import all this files in the global scope anymore.

---
+++ core/spec.js
@@ -0,0 +1,15 @@
+/*
+ * import desc/* as a module
+ */
+
+var fs = require('fs');
+
+eval(fs.readFileSync('core/desc/room_desc.js')+'');
+exports.DEFAULT_STATUS = DEFAULT_STATUS;
+exports.ROOM = ROOM;
+
+eval(fs.readFileSync('core/desc/player_desc.js')+'');
+exports.PLAYER = PLAYER;
+
+eval(fs.readFileSync('core/desc/event_desc.js')+'');
+exports.EVENT = EVENT;
