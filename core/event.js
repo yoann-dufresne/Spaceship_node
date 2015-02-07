142ab0b6eba3229cbd34dc83642b5cbd36043ff0
Marc <Marc.duez@lifl.fr>
2015-02-07 14:01:06 +0100

    event.js : improve event > auto-find a room to occur >cross reference with spaceship >store event_id

--- core/event.js
+++ core/event.js
@@ -3,17 +3,18 @@ var spec = require('./spec');
 module.exports.Event = Event;
 module.exports.BasicEvent = BasicEvent;
 
-function Event (id, type) {
+function Event (type, id, spaceship) {
 
-    this.init(id, type);
+    this.init(type, id, spaceship);
 }
 
 Event.prototype = {
     
-    init : function (id, type) {
+    init : function (type, id, spaceship) {
         console.log("-------new_event : "+type+"------");
         this.type = type;
         this.id = id;
+        this.spaceship = spaceship;
         this.effect = [];
         this.oxygen = 0;
         this.speed = 0;
@@ -48,12 +49,22 @@ Event.prototype = {
     
 }
 
-function BasicEvent (id, type, room_id) {
+function BasicEvent (type, id, spaceship) {
 
     // call the Event constructor. I think is cleaner than call the init() method.
-    Event.call(this, id, type);
-
-    this.room_id = room_id
+    Event.call(this, type, id, spaceship);
+    
+    //select a room among available one
+    var rooms = this.spaceship.getAvailableRoom();
+    
+    //if not enough available room for this event
+    //active -> false will remove it 
+    if (rooms.length < 1) {
+        this.active = false
+    }else{
+        this.room_id = rooms[Math.floor(Math.random()*rooms.length)];
+        this.applyEffect()
+    }
 }
 
 BasicEvent.prototype = Object.create(Event.prototype);
@@ -66,19 +77,21 @@ BasicEvent.prototype.toJson = function () {
     var json = Event.prototype.toJson.call(this);
  
     // extend it
-    json.room_id = this.room_id;
+    json.room_id = this.spaceship.getAvailableRoom();
 
     return json;
 }
 
-BasicEvent.prototype.applyEffect = function (spaceship) {
+BasicEvent.prototype.applyEffect = function () {
     if (this.active){
-        spaceship.room[this.room_id].changeStatus('disabled');
+        this.spaceship.room[this.room_id].changeStatus('disabled');
+        this.spaceship.room[this.room_id].available = false;
     }
 }
 
-BasicEvent.prototype.solve = function (spaceship, player_id) {
+BasicEvent.prototype.solve = function (player_id) {
     this.active = false
-    spaceship.room[this.room_id].changeStatus('enabled')
+    this.spaceship.room[this.room_id].changeStatus('enabled')
+    this.spaceship.room[this.room_id].available = true;
 }
 
--- core/player.js
+++ core/player.js
@@ -1,17 +1,19 @@
-module.exports.Player = Player
+module.exports.Player = Player;
 
-function Player(type) {
-    console.log("-------new_player : "+type+"------")
-    this.type = type
+function Player(type, id) {
+    console.log("-------new_player : "+type+"------");
+    this.type = type;
+    this.id = id;
 }
 
 Player.prototype = {
     
     toJson : function () {
         var json = {
-            'type' : this.type
-        }
-        return json
+            'type' : this.type,
+            'id' : this.id
+        };
+        return json;
     }
     
 }
