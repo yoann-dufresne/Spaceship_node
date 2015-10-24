984d5609f4d998044a060a9f84fefa06a09ae565
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-10-24 12:28:11 +0200

    easy fire

--- client/js/room.js
+++ client/js/room.js
@@ -8,17 +8,17 @@ function release_keyboard(){
 
 function Room() {
     this.lock_div = document.getElementById("lock")
-    this.input=""  
+    this.input=""
     this.init()
-    this.lock();  
+    this.lock();
     this.print();
-	
+
 	this.flag = false;
 }
 
 
 Room.prototype = {
-    
+
     init : function () {
         var self = this;
 		this.comunicate = new ServerCommunication();
@@ -28,14 +28,14 @@ Room.prototype = {
         img.src = "img/parasites.jpg";
         img.id = "parasites";
         document.getElementById("mainFrame").appendChild(img);
-        
+
 		setInterval(function(){self.update()}, 1000);
 	},
-	
+
 	update : function () {
 		var self = this;
-		this.comunicate.askServer("/spaceship", 
-			    {"command":"getStatus"}, 
+		this.comunicate.askServer("/spaceship",
+			    {"command":"getStatus"},
 			    function (spaceshipData) {
 					self.data = JSON.parse(spaceshipData);
 					self.display_room_selector();
@@ -47,7 +47,7 @@ Room.prototype = {
                         self.imgPara.style.opacity = 0;
 			    });
 	},
-    
+
 	display_room_selector : function () {
 		var self = this;
 		if (!this.flag & this.data.room.length > 0) {
@@ -64,15 +64,15 @@ Room.prototype = {
 			this.flag = true;
 		}
 	},
-	
+
     defineRoom : function(room){
         this.roomID = room.attr('id').replace("room_", "");
-        $("#menu").fadeOut(500) 
-        
+        $("#menu").fadeOut(500)
+
         $("#door_label").html(this.data.room[this.roomID].type)
-        $("#room_name").html(this.data.room[this.roomID].type) 
+        $("#room_name").html(this.data.room[this.roomID].type)
     },
-	
+
 	/*
     update : function(){
 		var self = this;
@@ -94,8 +94,8 @@ Room.prototype = {
             self.checkEvent()
         },800);
     },
-	
-    
+
+
     //ferme la porte
     close : function() {
 	var self=this;
@@ -109,19 +109,19 @@ Room.prototype = {
 	this.print()
         setTimeout(function(){self.lock()},1500);
     },
-    
+
     //
     checkEvent : function () {
         var self=this;
-        
+
 		var instruction = {
 			'Fire' : " Guide la lance à incendie avec les flèches du clavier pour récupérer les seaux et éteindre les incendies. Maintenir CTRL pour accélérer. ",
 			'Alien' : " Tue les Aliens en tapant leurs noms sans faute. ",
 			'Hack' : " Trouve le code secret de 4 chiffres et communique le au Command-Center. Vert chiffre bien placé. Orange chiffre présent mal placé. Rouge chiffre absent. "
 		}
-		
+
 		this.event = -1;
-		
+
 		for (var i in this.data.event){
 			if (this.data.event[i].room_id == this.roomID){
 				this.event = i;
@@ -129,34 +129,34 @@ Room.prototype = {
 				this.event_type = this.data.event[i].type;
 			}
 		}
-		
+
         //check if event exist
         if (this.event == -1){
             $("#room_event").html("room status : OK")
             setTimeout(function(){
                 self.close()
             },1500);
-            
+
             return
         }
-        
+
         if (EVENT[this.data.event[this.event].type].player.indexOf(this.data.player[this.player_id].type) == -1) {
-			$("#room_event").html( EVENT[this.data.event[this.event].type].player 
-				+ " needed to solve " 
+			$("#room_event").html( EVENT[this.data.event[this.event].type].player
+				+ " needed to solve "
 				+ EVENT[this.data.event[this.event].type].type )
 			setTimeout(function(){
 				self.close()
 			},1500);
-			
+
 			return
         }
 
         var callback = function(arg) { self.solve(arg) }//double closure ?
-        
+
 		$("#room_event").html(instruction[this.event_type]);
         switch(this.event_type) {
 			case "Fire":
-				
+
                 var node = document.getElementById('game')
 			    fire.start({
                     node: node,
@@ -165,6 +165,16 @@ Room.prototype = {
                     canvasHeight: node.clientHeight,
                     mapWidth: Math.floor(node.clientWidth / 50),
                     mapHeight: Math.floor(node.clientHeight / 50),
+                    snakeSize: 6,
+                    waterStock: 3,
+                    scoreToWin: 3,
+                    fireSpawnProba: 1,
+                    waterSpawnProba: 0,
+                    maxWatersNumber: 3,
+                    maxFiresNumber: 1,
+                    startFiresNumber: 1,
+                    startWatersNumber: 0,
+                    interval: 140,
                 }); 
 				break;
 			case "Alien":
@@ -177,12 +187,12 @@ Room.prototype = {
 				});
 				break;
 			default:
-				this.g = new MiniGames("game", callback);  
+				this.g = new MiniGames("game", callback);
 				break;
 		}
-           
+
     },
-    
+
     solve :function (arg) {
         $("#room_event").html("YEAHHHHH!!!")
         $("#game").html("")
@@ -191,28 +201,28 @@ Room.prototype = {
 			'event_id' : this.event_id,
 			'command': 'solve'
 		}
-		
+
 		if (typeof arg != 'undefined'){
 			args["arg"]=arg;
 		}
-		
+
 		this.comunicate.askServer('/event', args);
         this.close()
-        
+
     },
-    
+
     //rend le controle au verrou
     lock : function() {
         var self=this
         //take control of input
         document.onkeydown = function (e) { self.checkKey(e);}
     },
-    
+
     //input event handler
     checkKey : function(e) {
         e = e || window.event;
         e.preventDefault()
-        
+
         if (e.keyCode == 8){
             this.input = this.input.substring(0, this.input.length-1)
         }else if (e.keyCode == 13){
@@ -225,23 +235,23 @@ Room.prototype = {
         }
         this.print()
     },
-    
+
     //ecran du verrou
     print : function (text){
         this.lock_div.innerHTML = this.input + "<span class='bar'>_</span>"
     },
-    
+
     //tentative d'ouverture
     unlock : function () {
         var self = this;
         this.input = this.input.toUpperCase();
-		
+
 		this.player_id = -1;
-		
+
 		for (var i in this.data.player){
 			if (this.data.player[i].password.toUpperCase() == this.input)  this.player_id = i;
 		}
-		
+
         if (this.player_id != -1){
             this.player = this.data.player[i].type;
             this.print()
@@ -255,17 +265,17 @@ Room.prototype = {
                 self.lock();
             },800);
         }
-        
+
         this.input = ""
     }
-    
+
 }
 
 
 
 
 
-/* s'occupe de lancer les minigames a la suite 
+/* s'occupe de lancer les minigames a la suite
  * id => la fenetre dans laquelle les jeux s'affiche
  * n  => le nombre de jeux a résoudre
  */
@@ -274,7 +284,7 @@ function MiniGames (id, callback) {
 	this.id=id;
     this.callback = callback;
 	this.gameList = [Game1,Game2,Game3]
-	
+
 	this.nextGame();
 }
 
@@ -283,16 +293,16 @@ MiniGames.prototype = {
 	nextGame : function () {
 		var self = this;
 		this.n--;
-		
+
 		//custom callback for the last game
 		var callback = function() { self.nextGame() }
 		if (this.n <= 0) callback = function() { self.end() }
-		
+
 		//choose random minigame
 		var g = this.gameList[ Math.floor(Math.random()*this.gameList.length) ]
 		this.m = new g(this.id, callback)
 	},
-	
+
 	end : function () {
         this.callback()
 	}
@@ -306,26 +316,26 @@ MiniGames.prototype = {
 function Game1 (id, callback) {
 	this.id = id;
 	this.callback = callback;
-    this.init();    
+    this.init();
 }
 
 Game1.prototype = {
- 
+
     init : function () {
         var self = this;	//javascript magic
         this.goal = 20;		//nombre de hit a effectuer
 		this.count = 0;
 		this.up = true;		//check si la touche n'est pas en hold
         this.key = 97 + Math.floor(Math.random()*26) //choose random letter
-		
+
         console.log("press " + this.key + "  " + String.fromCharCode(this.key))
 		this.display();
-		
+
 		//take control of input
         document.onkeypress = function (e) { self.checkKey(e);}
 		document.onkeyup = function () { self.up = true }
     },
-	
+
 	display : function () {
 		this.div = jQuery('<div/>', {
 			html: "hit <span class='key blink1'>" + String.fromCharCode(this.key) + "</span>  ",
@@ -334,21 +344,21 @@ Game1.prototype = {
 		})
 		.appendTo("#"+this.id)
 		.slideDown(200);
-		
+
 		this.gauge = jQuery('<div/>', {class: 'gauge'})
 		.appendTo(this.div)
-		
+
 		this.level = jQuery('<div/>', {class: 'gauge_level'})
 		.appendTo(this.gauge)
-		
+
 	},
-    
-    checkKey : function(e) {	
+
+    checkKey : function(e) {
         e = e || window.event;
         e.preventDefault()
-	
+
 	var key = e.keyCode
-	if (key == 0) key = e.which		
+	if (key == 0) key = e.which
         if(this.key == String.fromCharCode(key.toString()).toLowerCase().charCodeAt(0).toString()  & this.up){
             this.count++;
 			this.level.css("width", "" + Math.floor( (this.count/this.goal)*100 ) + "%");
@@ -356,13 +366,13 @@ Game1.prototype = {
         }
 		if (this.count>=this.goal) this.end()
     },
-	
+
 	end : function () {
 		release_keyboard()
 		this.div.addClass("inactive")
 		this.callback()
 	}
-    
+
 }
 
 
@@ -370,24 +380,24 @@ Game1.prototype = {
 function Game2 (id, callback) {
 	this.id =id;
 	this.callback = callback;
-    this.init();    
+    this.init();
 }
 
 Game2.prototype = {
- 
+
     init : function () {
         var self = this;	//javascript magic
         this.goal = 60;	//nombre de hit a effectuer
 		this.count = 0;
         this.key = 97 + Math.floor(Math.random()*26) //choose random letter
-		
+
         console.log("hold " + this.key + "  " + String.fromCharCode(this.key))
 		this.display();
-		
+
 		//take control of input
         document.onkeypress = function (e) { self.checkKey(e);}
     },
-	
+
 	display : function () {
 		this.div = jQuery('<div/>', {
 			html: "hold <span class='key'>" + String.fromCharCode(this.key) + "</span>  ",
@@ -396,44 +406,44 @@ Game2.prototype = {
 		})
 		.appendTo("#"+this.id)
 		.slideDown(200);
-		
+
 		this.gauge = jQuery('<div/>', {class: 'gauge'})
 		.appendTo(this.div)
-		
+
 		this.level = jQuery('<div/>', {class: 'gauge_level'})
 		.appendTo(this.gauge)
 	},
-    
-    checkKey : function(e) {	
+
+    checkKey : function(e) {
         e = e || window.event;
         e.preventDefault()
 
 	var key = e.keyCode;
 	if (key==0) key= e.which;
-        
+
         if(this.key == String.fromCharCode(key.toString()).toLowerCase().charCodeAt(0).toString() ){
             this.count++;
 			this.level.css("width", "" + Math.floor( (this.count/this.goal)*100 ) + "%");
         }
 		if (this.count>=this.goal) this.end()
     },
-	
+
 	end : function () {
 		release_keyboard()
 		this.div.addClass("inactive")
 		this.callback()
 	}
-    
+
 }
 
 function Game3 (id, callback) {
     this.id =id;
     this.callback = callback;
-    this.init();    
+    this.init();
 }
 
 Game3.prototype = {
- 
+
     init : function () {
         var self = this;    //javascript magic
         this.goal = 20; //nombre de hit a effectuer
@@ -442,13 +452,13 @@ Game3.prototype = {
         this.key2 = 97 + Math.floor(Math.random()*26) //choose random letter
 	while (this.key1 == this.key2) this.key2 = 97 + Math.floor(Math.random()*26)
         this.previousKey = 0;
-        
+
         this.display();
-        
+
         //take control of input
         document.onkeypress = function (e) { self.checkKey(e);}
     },
-    
+
     display : function () {
         this.div = jQuery('<div/>', {
             html: "altern keys <span class='key'>" + String.fromCharCode(this.key1) + "</span>  and <span class='key'>" + String.fromCharCode(this.key2) + "</span> ",
@@ -457,35 +467,35 @@ Game3.prototype = {
         })
         .appendTo("#"+this.id)
         .slideDown(200);
-        
+
         this.gauge = jQuery('<div/>', {class: 'gauge'})
         .appendTo(this.div)
-        
+
         this.level = jQuery('<div/>', {class: 'gauge_level'})
         .appendTo(this.gauge)
     },
-    
-    checkKey : function(e) {    
+
+    checkKey : function(e) {
         e = e || window.event;
         e.preventDefault()
-        
+
 	var key = e.keyCode;
 	if (key==0) key = e.which
-        var k = String.fromCharCode(key.toString()).toLowerCase().charCodeAt(0).toString() 
+        var k = String.fromCharCode(key.toString()).toLowerCase().charCodeAt(0).toString()
         if ( ( this.key1 == k || this.key2 == k ) && k != this.previousKey) {
-            this.previousKey = key.toString() 
+            this.previousKey = key.toString()
             this.count++;
             this.level.css("width", "" + Math.floor( (this.count/this.goal)*100 ) + "%");
         }
         if (this.count>=this.goal) this.end()
     },
-    
+
     end : function () {
 	release_keyboard()
         this.div.addClass("inactive")
         this.callback()
     }
-    
+
 }
 
 var room = new Room();
